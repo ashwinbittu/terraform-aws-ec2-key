15ec9542d3e9d772c0a06ba66e6570fdb1981f6a
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-23 19:12:10 +1000

    Add files via upload

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
-output "aws_ec2_keypair_name" {
-  value       = aws_key_pair.keypair.key_name
+output "public_key" {
+  value = aws_key_pair.this.public_key
+}
+
+output "private_key" {
+  value = tls_private_key.this.private_key_pem
+}
+
+output "key-name" {
+  value = aws_key_pair.this.key_name
 }
